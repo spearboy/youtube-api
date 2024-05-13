e66dd9e13ab3e4659943675309e2b80ee221cbf9
spearboy <tsagaanaa123@gmail.com>
2024-05-13 12:31:43 +0900

    페이지 완성

---
+++ src/pages/Website.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+    <div  style={{ display: 'flex', gap: '20px'}}>
+        <a href="/" style={{ color: 'black'}}>home</a>
+        <a href="/today" style={{ color: 'black'}}>today</a>
+        <a href="/developer" style={{ color: 'black'}}>developer</a>
+        <a href="/webd" style={{ color: 'black'}}>webd</a>
+        <a href="/website" style={{ color: 'red'}}>website</a>
+        <a href="/gsap" style={{ color: 'black'}}>gsap</a>
+        <a href="/port" style={{ color: 'black'}}>port</a>
+        <a href="/youtube" style={{ color: 'black'}}>youtube</a>
+        <a href="/channel" style={{ color: 'black'}}>channel</a>
+        <a href="/video" style={{ color: 'black'}}>video</a>
+        <a href="/search" style={{ color: 'black'}}>search</a>
+    </div>
+  )
+}
+
+export default Website
